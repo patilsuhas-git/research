db7e97be15ea6bf9f9d56b8c341fc9242a6e6cbb
Suhas Patil <suhas.patil@salesforce.com>
2022-11-03 12:12:54 -0500

    First commit

---
+++ src/test/java/com/example/research/login/ResearchLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.research.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchLoginApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
